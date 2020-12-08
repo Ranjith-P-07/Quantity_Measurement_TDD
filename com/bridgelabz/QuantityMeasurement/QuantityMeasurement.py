91e141c2b2870d87dc25a8df22c99081c52a93a7
Ranjith P <Ranjithroy704@gmail.com>
2020-12-09 00:50:48 +0530

    "Add: added Condition to check for Float Values"

--- com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
+++ com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
@@ -1,5 +1,10 @@
+from com.bridgelabz.QuantityMeasurement.QuantityMeasurementError import QuantityMeasurementError
+
+
 class Feet:
     def __init__(self, feet):
+        if type(feet) != float:
+            raise QuantityMeasurementError("Entered Value is not Float type")
         self.feet = feet
 
     def __eq__(self, other):
