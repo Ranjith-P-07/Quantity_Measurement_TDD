7550c6ad1265f70461a8c9b934a8fb075663054c
Ranjith P <Ranjithroy704@gmail.com>
2020-12-09 00:48:09 +0530

    "Add: added some more Test Cases for Feet "

--- com/bridgelabz/Quantity_testing/test_Quantity.py
+++ com/bridgelabz/Quantity_testing/test_Quantity.py
@@ -1,4 +1,6 @@
 from com.bridgelabz.QuantityMeasurement.QuantityMeasurement import Feet
+from com.bridgelabz.QuantityMeasurement.QuantityMeasurementError import QuantityMeasurementError
+import pytest
 
 
 def test_givenZeroFtAndZeroFt_whenCompared_returns_true():
@@ -10,3 +12,18 @@ def test_givenZeroFtAndZeroFt_whenCompared_returns_true():
 def test_givenZeroFtAndNone_whenCompared_returns_False():
     first_feet = Feet(0.0)
     assert first_feet is not None
+
+
+def test_givenZeroFtAndNonFloatType_WhenCompared_raise_Error():
+    with pytest.raises(QuantityMeasurementError):
+        first_feet = Feet(0.0)
+        second_feet = Feet(0)
+        assert first_feet == second_feet
+
+
+def test_givenDifferentFtValues_WhenCompared_ShouldReturn_False():
+    first_feet = Feet(1.0)
+    second_feet = Feet(2.0)
+    assert (first_feet == second_feet) == False
+
+
