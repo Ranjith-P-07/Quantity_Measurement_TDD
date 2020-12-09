2f3bef467adc95ca13da17871205cd910165e355
Ranjith P <Ranjithroy704@gmail.com>
2020-12-09 13:35:49 +0530

    "Add: Added Comparision Test Cases for Feet and Yard"

--- com/bridgelabz/Quantity_testing/test_Quantity.py
+++ com/bridgelabz/Quantity_testing/test_Quantity.py
@@ -3,12 +3,14 @@ from com.bridgelabz.QuantityMeasurement.QuantityMeasurementError import Quantity
 import pytest
 
 
+# Comparing Two same Feet Values and Should return True
 def test_givenZeroFtAndZeroFt_whenCompared_returns_true():
     first_feet = Feet(0.0)
     second_feet = Feet(0.0)
     assert first_feet == second_feet
 
 
+# Comparing If We give only one value to Feet to check whether it is Returning False or not
 def test_givenZeroFtAndNone_whenCompared_returns_False():
     first_feet = Feet(0.0)
     assert first_feet is not None
@@ -49,3 +51,21 @@ def test_givenDifferentYardValues_WhenCompared_ShouldReturn_False():
     first_yard = Yard(1.0)
     second_yard = Yard(2.0)
     assert (first_yard == second_yard) == False
+
+
+def test_given_3Ft_And_1Yard_whenCompared_ShouldReturn_True():
+    first_feet = Feet(3.0)
+    second_yard = Yard(1.0)
+    assert first_feet == second_yard
+
+
+def test_given_1Ft_And_1Yard_WhenCompared_ShoulReturn_False():
+    first_feet = Feet(1.0)
+    second_yard = Yard(1.0)
+    assert first_feet != second_yard
+
+
+def test_given_1Yard_And_3Ft_WhenCompared_ShouldReturn_True():
+    first_yard = Yard(1.0)
+    second_feet = Feet(3.0)
+    assert first_yard == second_feet
