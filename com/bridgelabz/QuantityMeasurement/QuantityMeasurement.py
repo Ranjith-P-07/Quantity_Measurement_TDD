a21a9ce8436662110fe40ca64aa68a87153e6750
Ranjith P <Ranjithroy704@gmail.com>
2020-12-09 19:13:20 +0530

    "Add: Added Inch class for Inch to Feet/Yard conversion"

--- com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
+++ com/bridgelabz/QuantityMeasurement/QuantityMeasurement.py
@@ -3,23 +3,35 @@ from com.bridgelabz.QuantityMeasurement.QuantityMeasurementError import Quantity
 
 class Feet:
     def __init__(self, feet):
-        if type(feet) != float:
-            raise QuantityMeasurementError("Entered Value is not Float type")
         self.feet = feet
 
     def __eq__(self, other):
         if isinstance(other, Yard):
             return self.feet == other.yard * 3
+        if isinstance(other, Inch):
+            return other.inch == self.feet * 12
         return self.feet == other
 
 
 class Yard:
     def __init__(self, yard):
-        if type(yard) != float:
-            raise QuantityMeasurementError("Entered Value is not float type")
         self.yard = yard
 
     def __eq__(self, other):
         if isinstance(other, Feet):
             return other.feet == self.yard * 3
+        if isinstance(other, Inch):
+            return other.inch == self.yard * 36
         return self.yard == other
+
+
+class Inch:
+    def __init__(self, inch):
+        self.inch = inch
+
+    def __eq__(self, other):
+        if isinstance(other, Feet):
+            return self.inch == other.feet * 12
+        if isinstance(other, Yard):
+            return self.inch == other.yard * 36
+        return self.inch == other
