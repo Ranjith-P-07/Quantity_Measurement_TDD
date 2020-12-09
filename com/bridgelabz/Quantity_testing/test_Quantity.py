366f2343401fd35241dc521a128671d8bc488bec
Ranjith P <Ranjithroy704@gmail.com>
2020-12-09 11:48:32 +0530

    "Add: Added Test Cases for Yard Comparison"

--- com/bridgelabz/Quantity_testing/test_Quantity.py
+++ com/bridgelabz/Quantity_testing/test_Quantity.py
@@ -1,4 +1,4 @@
-from com.bridgelabz.QuantityMeasurement.QuantityMeasurement import Feet
+from com.bridgelabz.QuantityMeasurement.QuantityMeasurement import Feet, Yard
 from com.bridgelabz.QuantityMeasurement.QuantityMeasurementError import QuantityMeasurementError
 import pytest
 
@@ -27,3 +27,25 @@ def test_givenDifferentFtValues_WhenCompared_ShouldReturn_False():
     assert (first_feet == second_feet) == False
 
 
+def test_givenZeroYardAndZeroYard_whenCompared_returns_true():
+    first_yard = Yard(0.0)
+    second_yard = Yard(0.0)
+    assert first_yard == second_yard
+
+
+def test_givenZeroYardAndNone_whenCompared_returns_False():
+    first_yard = Yard(0.0)
+    assert first_yard is not None
+
+
+def test_givenZeroYardAndNonFloatType_WhenCompared_raise_Error():
+    with pytest.raises(QuantityMeasurementError):
+        first_yard = Yard(0.0)
+        second_yard = Yard(0)
+        assert first_yard == second_yard
+
+
+def test_givenDifferentYardValues_WhenCompared_ShouldReturn_False():
+    first_yard = Yard(1.0)
+    second_yard = Yard(2.0)
+    assert (first_yard == second_yard) == False
